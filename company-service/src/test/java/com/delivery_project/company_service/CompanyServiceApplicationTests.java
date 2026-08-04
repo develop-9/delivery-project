9ec641fa5d0e4028ca1c31b7749a3b7cea1cb762
rvbear <rvbear2000@gmail.com>
2026-08-04 16:22:29 +0900

    fix: company-service CI 테스트 환경 설정 수정

--- company-service/src/test/java/com/delivery_project/company_service/CompanyServiceApplicationTests.java
+++ company-service/src/test/java/com/delivery_project/company_service/CompanyServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.delivery_project.company_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CompanyServiceApplicationTests {
 
 	@Test
