e79e874f8e3af8300978fb516e1f0d2353b7b15b
TAEYOON EOM <djaxodbs0101@naver.com>
2026-08-05 12:21:17 +0900

    fix: JPA Auditing 설정 추가(임시)

--- delivery-service/src/main/java/com/delivery_project/delivery_service/global/config/JpaConfig.java
+++ delivery-service/src/main/java/com/delivery_project/delivery_service/global/config/JpaConfig.java
@@ -1,9 +1,22 @@
 package com.delivery_project.delivery_service.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Configuration
 @EnableJpaAuditing
-public class JpaConfig {
+public class JpaConfig { // 임시 처리
+
+    private static final UUID SYSTEM_USER_ID =
+            UUID.fromString("00000000-0000-0000-0000-000000000001");
+
+    @Bean
+    public AuditorAware<UUID> auditorProvider() {
+        return () -> Optional.of(SYSTEM_USER_ID);
+    }
 }
