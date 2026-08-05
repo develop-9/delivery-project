811d33a369c656e89236e7701d7f8a4783fd7781
TAEYOON EOM <djaxodbs0101@naver.com>
2026-08-06 00:28:27 +0900

    refactor: JPA 감사 시스템 ID 설정 통일

--- delivery-service/src/main/java/com/delivery_project/delivery_service/global/config/JpaConfig.java
+++ delivery-service/src/main/java/com/delivery_project/delivery_service/global/config/JpaConfig.java
@@ -1,5 +1,6 @@
 package com.delivery_project.delivery_service.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -12,11 +13,11 @@ import java.util.UUID;
 @EnableJpaAuditing
 public class JpaConfig { // 임시 처리
 
-    private static final UUID SYSTEM_USER_ID =
-            UUID.fromString("00000000-0000-0000-0000-000000000001");
+    @Value("${system.id}")
+    private UUID systemId;
 
     @Bean
     public AuditorAware<UUID> auditorProvider() {
-        return () -> Optional.of(SYSTEM_USER_ID);
+        return () -> Optional.of(systemId);
     }
 }
