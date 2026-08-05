03ec8873893abff79f48825cc5f5e262e45276d7
daengae <dladmsxor434@naver.com>
2026-08-05 12:43:50 +0900

    feat: DB/스키마 네이밍 변경

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -14,3 +14,5 @@
 --   user_schema / hub_schema / company_schema / order_schema / delivery_schema / slack_schema
 
 CREATE SCHEMA IF NOT EXISTS hub_schema;
+
+CREATE SCHEMA IF NOT EXISTS user_schema;
