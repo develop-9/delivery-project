7dcc00b8d6f11794d2e5a9405b557c5f6d2dfebb
rvbear <rvbear2000@gmail.com>
2026-08-07 01:30:40 +0900

    feat: Product Endpoint 변경

--- company-service/src/main/java/com/delivery_project/company_service/company/presentation/api_controller/ProductApiController.java
+++ company-service/src/main/java/com/delivery_project/company_service/company/presentation/api_controller/ProductApiController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductApiController implements ProductApi {
 
