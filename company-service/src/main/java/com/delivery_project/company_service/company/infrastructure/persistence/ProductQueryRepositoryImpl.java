d215ccced2c5d343adb0444380e62ed5c2f842a8
rvbear <rvbear2000@gmail.com>
2026-08-06 23:15:14 +0900

    feat: Product Infrastructure 구성

---
+++ company-service/src/main/java/com/delivery_project/company_service/company/infrastructure/persistence/ProductQueryRepositoryImpl.java
@@ -0,0 +1,20 @@
+package com.delivery_project.company_service.company.infrastructure.persistence;
+
+import com.delivery_project.company_service.company.domain.entity.Product;
+import com.delivery_project.company_service.company.domain.repository.ProductQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductQueryRepositoryImpl implements ProductQueryRepository {
+
+    private final SpringDataProductRepository springDataProductRepository;
+
+    public Optional<Product> findById(UUID productId) {
+        return springDataProductRepository.findById(productId);
+    }
+}
