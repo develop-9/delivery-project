577c4048284892b150eef968207928a4fc29b2d7
TAEYOON EOM <djaxodbs0101@naver.com>
2026-08-07 22:31:24 +0900

    fix: Advisory Lock을 통한 Delivery 중복 생성 방지 및 테스트 코드 수정

--- delivery-service/src/main/java/com/delivery_project/delivery_service/delivery/application/command_service/DeliveryPersistenceService.java
+++ delivery-service/src/main/java/com/delivery_project/delivery_service/delivery/application/command_service/DeliveryPersistenceService.java
@@ -1,6 +1,7 @@
 package com.delivery_project.delivery_service.delivery.application.command_service;
 
 import com.delivery_project.delivery_service.delivery.application.command.DeliveryCreateCommand;
+import com.delivery_project.delivery_service.delivery.application.port.DeliveryCreationLockPort;
 import com.delivery_project.delivery_service.delivery.application.result.DeliveryCreateResult;
 import com.delivery_project.delivery_service.delivery.application.result.DeliveryPath;
 import com.delivery_project.delivery_service.delivery.application.result.ReceiverInfo;
@@ -8,6 +9,8 @@ import com.delivery_project.delivery_service.delivery.domain.entity.Delivery;
 import com.delivery_project.delivery_service.delivery.domain.entity.DeliveryRoute;
 import com.delivery_project.delivery_service.delivery.domain.repository.DeliveryCommandRepository;
 import com.delivery_project.delivery_service.delivery.domain.repository.DeliveryRouteCommandRepository;
+import com.delivery_project.delivery_service.global.exception.BusinessException;
+import com.delivery_project.delivery_service.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +23,7 @@ public class DeliveryPersistenceService {
 
     private final DeliveryCommandRepository deliveryCommandRepository;
     private final DeliveryRouteCommandRepository deliveryRouteCommandRepository;
+    private final DeliveryCreationLockPort deliveryCreationLockPort;
 
     @Transactional
     public DeliveryCreateResult create(
@@ -27,6 +31,19 @@ public class DeliveryPersistenceService {
             ReceiverInfo receiver,
             DeliveryPath deliveryPath
     ) {
+        deliveryCreationLockPort.lock(command.orderId());
+
+        boolean alreadyExists =
+                deliveryCommandRepository
+                        .findByOrderIdAndDeletedAtIsNull(command.orderId())
+                        .isPresent();
+
+        if (alreadyExists) {
+            throw new BusinessException(
+                    ErrorCode.DELIVERY_ALREADY_EXISTS
+            );
+        }
+
         Delivery delivery = Delivery.create(
                 command.orderId(),
                 command.departureHubId(),
