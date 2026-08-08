c3dccee552fcaf4de50ea0d36e56830bd5ef8187
rvbear <rvbear2000@gmail.com>
2026-08-08 23:55:10 +0900

    feat: JWT 인증/인가 구현

--- company-service/src/main/java/com/delivery_project/company_service/global/config/SecurityConfig.java
+++ company-service/src/main/java/com/delivery_project/company_service/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.delivery_project.company_service.global.config;
 
+import com.delivery_project.company_service.global.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +10,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -37,26 +41,27 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // 인증 없이 접근 가능
                         .requestMatchers(
-                                // ===개발을 위한 권한 허용===
-                                "/api/v1/**",
+                                // 내부 API
                                 "/internal/v1/**",
-                                // ======================
 
                                 // actuator
                                 "/actuator/**",
 
                                 // swagger
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html"
                         ).permitAll()
                         // 나머지는 인증 필요
                         .anyRequest().authenticated()
                 )
-
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 // 기본 CORS 설정
                 .cors(Customizer.withDefaults());
 
         return http.build();
     }
-
 }
