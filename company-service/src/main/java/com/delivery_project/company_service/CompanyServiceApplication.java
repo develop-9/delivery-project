0e5615a6a3d69a5a2aa11a0063496e14b8670708
rvbear <rvbear2000@gmail.com>
2026-08-10 15:06:22 +0900

    fix: 불필요한 import문 제거

--- company-service/src/main/java/com/delivery_project/company_service/CompanyServiceApplication.java
+++ company-service/src/main/java/com/delivery_project/company_service/CompanyServiceApplication.java
@@ -3,7 +3,6 @@ package com.delivery_project.company_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
 @EnableFeignClients
 @SpringBootApplication
